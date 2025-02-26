a6925fed2389e2ea2bcfa900ae2dbff473ea8d00
Neal Terrell <nealdt@gmail.com>
2025-02-26 10:18:29 -0800

    Film struct example.

---
+++ Classes/film-struct/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+struct Film {
+    std::string title;
+    int releaseYear;
+    int duration;
+    std::string rating;
+}; // note the semi colon here.
+
+// Print a film, which is taken as a const reference because it is large,
+// we don't mutate it, and we can't deal with nullptr.
+void printFilm(const Film& film) {
+    std::cout << film.title << " (" << film.releaseYear << "), rated " << film.rating;
+}
+
+int main() {
+    Film f {"Dog Man", 2025, 87, "PG"};
+    printFilm(f);
+    return 0;
+}
