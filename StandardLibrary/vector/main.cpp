be48ff1a26b53507e393e376ec13c8da9d1e6556
Neal Terrell <nealdt@gmail.com>
2025-03-12 10:46:16 -0700

    vector pop_back, insert, erase.

--- StandardLibrary/vector/main.cpp
+++ StandardLibrary/vector/main.cpp
@@ -43,8 +43,9 @@ int main() {
     }
 
     // 2. Range-based for loop.
-    for (int d : digits) {
-        std::cout << d << std::endl;
+    for (int& d : digits) {
+     //   std::cout << d << std::endl;
+        d *= 2;
     }
     // BE CAREFUL HERE: d is a *copy* of each integer in the vector.
 
@@ -77,6 +78,77 @@ int main() {
     }
 
 
+    // Removing elements.
+    // pop_back(): removes the last element of the vector.
+    // Reminder: at this point, digits = {8, 6, 7, 5, -3, -1, 9, 100}.
+    digits.pop_back();
+    // Now, digits = {8, 6, 7, 5, -3, -1, 9}.
 
+    for (int i {0}; i < 2; ++i) {
+        digits.pop_back();
+    }
+    // digits = {8, 6, 7, 5, -3}
+
+    // To remove from an arbitrary position, we use erase().
+    // erase(itr): removes the element pointed to by the given iterator.
+    digits.erase(digits.begin());
+    // Removes the 8.
+
+    // To identify an arbitrary index, we can add an integer to the begin() iterator.
+    digits.erase(digits.begin() + 2); // erases index 2.
+    // digits = {6, 7, -3}
+
+
+    // To insert at an arbitrary position, we use insert() simiarly to erase().
+    // insert(itr, value): insert the value at the given iterator position, shifting
+    // values at the existing position or after it to the right.
+    digits.insert(digits.begin() + 1, 99); // insert 99 at index 1.
+    // digits = {6, 99, 7, -3}
+    digits.insert(digits.begin() + 3, 200);
+    // digits = {6, 99, 7, 200, -3}.
+
+
+
+    // FINAL LESSON:
+    // Like *all other variables*, vectors are COPIED when assigned to another variable.
+    std::vector<int> c {digits};
+    // The vector copy constructor sets up c with its own array that copies each element
+    // of digits.
+    // Changes to c don't affect digits.
+    c.pop_back();
+    // digits still has 5 elements.
+
+    // This is why we pass vectors by reference to functions; either const if we don't mutate,
+    // or non-const if we do.
     return 0;
+}
+
+
+int findLargest(const std::vector<int>& values) {
+    int largest { values[0] };
+    for (auto itr {values.begin()}; itr != values.end(); ++itr) {
+        if (*itr > largest) {
+            largest = *itr;
+        }
+    }
+    return largest;
+}
+
+
+bool isOrdered(const std::vector<int>& values) {
+    for (size_t i = 0; i < values.size() - 1; ++i) {
+        if (values[i] > values[i+1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+std::vector<int> reverseCopy(const std::vector<int>& values) {
+    std::vector<int> temp {};
+
+    for (auto itr = values.rbegin(); itr != values.rend(); ++itr) {
+        temp.push_back(*itr);
+    }
+    return temp;
 }
