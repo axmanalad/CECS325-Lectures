21b90e895bdcfcfbd74c039362111d35f3e9a3c5
Neal Terrell <nealdt@gmail.com>
2025-04-14 12:00:30 -0700

    Minor tweak.

--- Processes/fork/main.cpp
+++ Processes/fork/main.cpp
@@ -9,17 +9,15 @@ int main() {
 
     int cid {fork()};
     if (cid == 0) {
+        // "Sleep_for" will cause this program to pause for X seconds.
+        // I'm doing this just so we can see this program in our process/task manager.
         std::this_thread::sleep_for(std::chrono::seconds(10));
         // This is in the new ("child") process. This is where we exec our new program.
         if (execl("/bin/ls", "ls", "-l", static_cast<char*>(nullptr)) == -1) {
             std::cout << "There was an error when trying to exec" << std::endl
                       << "Error number " << errno << std::endl;
         }
-
-        // "Sleep_for" will cause this program to pause for X seconds.
-        // I'm doing this just so we can see this program in our process/task manager.
-        std::this_thread::sleep_for(std::chrono::seconds(30));
-        // After this line, the child process will skip the "else", return 0, and then terminate.
+        // After this previous line, the child process will skip the "else", return 0, and then terminate.
     }
     else {
         std::cout << "cid " << cid << std::endl;
